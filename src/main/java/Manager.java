29c685ee08fa0fd475d03bdcc7202c0d309efe87
enricozorzi <fazioni98>
2020-05-13 17:23:31 +0200

    menager

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,27 +1,15 @@
 import com.google.gson.Gson;
 
 public class Manager {
-    public static class Container {
-        public CaratteristicheProdotto caratteristicheProdotto;
-        public Categoria categoria;
-        public FormaDiPagamento formaDiPagamento;
-        public Indirizzo indirizzo;
-        public Ordine ordine;
-        public Prodotto prodotto;
-        public RuoloResponsabile ruoloResponsabile;
-        public TesseraFedelta tesseraFedelta;
-        public UtenteCliente utenteCliente;
-        public UtenteResponsabile utenteResponsabile;
 
-    }
 
     public String objectToJson(Object obj){
             Gson gson = new Gson();
             return gson.toJson(obj);
     }
 
-    public Container jsonToObject(String json){
+    public <T> T jsonToObject(String json, Class<T> obj){
         Gson gson = new Gson();
-        return gson.fromJson(json, Container.class );
+        return gson.fromJson(json, obj );
     }
 }
