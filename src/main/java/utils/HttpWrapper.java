eb759c3b7ac843c8e45535b2fb27ba47e17052a3
Mattia Burati <mattiaburati@live.it>
2020-05-15 15:52:54 +0200

    Added Login Method

--- src/main/java/utils/HttpWrapper.java
+++ src/main/java/utils/HttpWrapper.java
@@ -1,6 +1,32 @@
 package utils;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
 public class HttpWrapper {
+    private String uri = "http://127.0.0.1:5000";
+    private String r= "";
+    public String login(String username,String password) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        try{
+            HttpGet request = new HttpGet(uri+"/login?"+username+"&"+password);
+            request.addHeader(HttpHeaders.USER_AGENT,"JAVACLIENT");
+            CloseableHttpResponse response = httpClient.execute(request);
+            System.out.println(response.getStatusLine().toString());
+        }
+        finally{
+            httpClient.close();
+        }
+        return r;
+    }
 
     //TODO user access method. return string, parameters email, password
 
--- src/test/java/TestExample.java
+++ src/test/java/TestExample.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+import utils.HttpWrapper;
+
+import java.io.IOException;
+
 public class TestExample {
+    @Test
+    public void test() throws IOException {
+        HttpWrapper http = new HttpWrapper();
+        http.login("Pippo","Franco");
+    }
+
 }
