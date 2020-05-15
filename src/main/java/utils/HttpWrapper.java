91331104f00277b2f10af3331b682af597a3a12d
luca <luca.gugole99@gmail.com>
2020-05-15 15:21:25 +0200

    nothing important

---
+++ src/main/java/utils/HttpWrapper.java
@@ -0,0 +1,25 @@
+package utils;
+
+public class HttpWrapper {
+
+    //TODO user access method. return string, parameters email, password
+
+    //TODO user creation. return boolean, parameters Utente
+
+    //TODO get products per availability
+
+    //TODO get products per name
+
+    //TODO get products per category
+
+    //TODO get products per tag
+
+    //TODO order sending, return boolean, parameters Ordine
+
+    //TODO products adding
+
+    //TODO products removing
+
+    //TODO request all user's orders
+
+}
