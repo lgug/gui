7747bbf51f0110813d3974918f408284c10e4a17
enricozorzi <fazioni98>
2020-05-18 16:49:13 +0200

    FXML

---
+++ src/main/java/gui/PopupCart.java
@@ -0,0 +1,32 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PopupCart extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemClassLoader().getResource("popup.fxml"));
+        AnchorPane pane = loader.load();
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("ciao");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+
+
+
+
+
