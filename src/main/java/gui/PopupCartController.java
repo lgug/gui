a10ce0d0519615bf5209d93bd6a9994c8ac9802e
enricozorzi <enricovrz2811@gmail.com>
2020-05-26 17:25:21 +0200

    Cart

--- src/main/java/gui/PopupCartController.java
+++ src/main/java/gui/PopupCartController.java
@@ -44,7 +44,7 @@ public class PopupCartController<TextView> {
     public ChoiceBox<String> choicePagamento;
     private UtenteCliente utenteCliente;
     private ObservableList<Prodotto> list = FXCollections.observableArrayList(
-            new Prodotto(8984,"banana", "chiquita", CaratteristicheProdotto.BIOLOGICO, Categoria.FRUTTA_VERDURA,2,10,1,"bananas.png"),
+            new Prodotto(8984,"banana", "chiquita", CaratteristicheProdotto.BIOLOGICO, Categoria.FRUTTA_VERDURA,2,0,1,"bananas.png"),
             new Prodotto(989,"cherry", "fruttissima",CaratteristicheProdotto.VEGAN,Categoria.FRUTTA_VERDURA,10,4,1,"cherries.png"),
             new Prodotto(4454,"sushi", "china express", CaratteristicheProdotto.BIOLOGICO, Categoria.FRUTTA_VERDURA,2,7,1,"sushi.png"),
             new Prodotto(8984,"banana", "chiquita", CaratteristicheProdotto.BIOLOGICO, Categoria.FRUTTA_VERDURA,2,10,1,"bananas.png")
@@ -114,26 +114,27 @@ public class PopupCartController<TextView> {
     }
 
     public void handleBuyButtonAction(MouseEvent mouseEvent) throws Exception {
-        for(Prodotto prodotto: list)
-            if(prodotto.getQuantita()> prodotto.getDisponibilita()){
+        boolean check= true;
+        for (Prodotto prodotto : list)
+            if (prodotto.getQuantita() > prodotto.getDisponibilita()) {
                 ErrorPageQuantita errorPageQuantita = new ErrorPageQuantita();
                 errorPageQuantita.start(new Stage());
                 list.remove(prodotto);
                 initialize();
-                break;
+                check = false;
             }
-
-        Ordine ord = new Ordine();
-        ord.setData(new SimpleDateFormat("dd/MM/yyyy").format(new Date()));
-        List<ProdottoSemplificato> listprodsempl = new ArrayList<ProdottoSemplificato>();
-        for(Prodotto prodotto :list){
-            listprodsempl.add(new ProdottoSemplificato(prodotto.getId(),prodotto.getQuantita()));
+        if (check) {
+            Ordine ord = new Ordine();
+            ord.setData(new SimpleDateFormat("dd/MM/yyyy").format(new Date()));
+            List<ProdottoSemplificato> listprodsempl = new ArrayList<ProdottoSemplificato>();
+            for (Prodotto prodotto : list) {
+                listprodsempl.add(new ProdottoSemplificato(prodotto.getId(), prodotto.getQuantita()));
+            }
+            ord.setProdotti(listprodsempl);
+            Random rand = new Random();
+            ord.setID(String.valueOf(rand.nextInt()));
+            HttpWrapper http = new HttpWrapper();
+            http.addOrdine("1", ord);
         }
-        ord.setProdotti(listprodsempl);
-        Random rand = new Random();
-        ord.setID(String.valueOf(rand.nextInt()));
-        HttpWrapper http = new HttpWrapper();
-        http.addOrdine("1", ord);
     }
-
 }
